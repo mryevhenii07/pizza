8d939ded318ee164a42dd39989b7b2676ec354a9
mryevhenii07 <mrit.works07@gmail.com>
2022-06-02 13:54:29 +0300

    changed component notfound

--- src/component/NotFound/NotFoundBlock.jsx
+++ src/component/NotFound/NotFoundBlock.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import s from "./NotFoundBlock.module.scss";
 
 const NotFoundBlock = () => {
   return (
-    <div className={s.wrap}>
-      <span className={s.span}>😒</span>
-      <h1> NotFoundBlock</h1>
+    // <div className={s.wrap}>
+    //   <span className={s.span}>😒</span>
+    //   <h1> NotFoundBlock</h1>
+    // </div>
+    <div className={s.container}>
+      <div className={s.context}>
+        <h2>404</h2>
+        <h4>Opps! Page not found</h4>
+        <p>
+          The page you were looking for doesnt exist.You may have mistyped the
+          address or the page may have moved.
+        </p>
+        <Link className={s.link} to="/">
+          Back to home
+        </Link>
+      </div>
     </div>
   );
 };
